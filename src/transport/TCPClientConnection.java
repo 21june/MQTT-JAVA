b1b96b01b139ab9c7771bf6349be860a1056e654
21JUNE <21june_@naver.com>
2017-02-09 10:55:17 +0900

    previous work

--- src/transport/TCPClientConnection.java
+++ src/transport/TCPClientConnection.java
@@ -1,17 +1,7 @@
 package transport;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-
-import constants.QoS;
 
 public class TCPClientConnection {
 
